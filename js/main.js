bab5c074daeea75e393e35a870a2db016641d4c6
Emas <emas.dev@gmail.com>
2021-03-14 23:12:19 -0600

    - Add find location via gps and by city name

--- js/main.js
+++ js/main.js
@@ -4,9 +4,103 @@ let weather1 = new Weather({
   container: document.getElementById("weather-mexico")
 });
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Mexico&appid=8ca3d179747703e653793ebb8935cbbf')
+let weather2 = new Weather({
+  container: document.getElementById("weather-location")
+});
+
+const appId = "8ca3d179747703e653793ebb8935cbbf";
+const location = {
+  latitude: 19.363661, 
+  longitude: -99.173382
+}
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${appId}`)
   .then(response => response.json())
   .then(data => {
     weather1.data = data;
     weather1.render();
   });
+
+window.onload = () => {
+  initEvents();
+}
+
+let isMenuVisible = true;
+const sections = {
+  menu: document.getElementById("section-menu"),
+  location: document.getElementById("section-location"),
+}
+
+function initEvents(){
+
+  const checkboxes = document.getElementsByClassName("radio");
+  for(const checkbox of checkboxes){
+    checkbox.addEventListener("click", (e) => {
+      weather1.grades = e.target.value;
+      weather1.render();
+      weather2.grades = e.target.value;
+      weather2.render();
+    });
+  }
+
+  document.getElementById("get-current-location")
+  .addEventListener("click", () => {
+    getLocation().then(location => {
+      console.log(location);
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${appId}`)
+      .then(response => response.json())
+      .then(data => {
+        weather2.data = data;
+        weather2.render();
+      });
+    })
+    toogleMenu();
+  });
+
+  document.getElementById("get-city-location")
+  .addEventListener("click", () => {
+
+      const city = document.getElementById("city-input").value;
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}`)
+      .then(response => response.json())
+      .then(data => {
+        weather2.data = data;
+        weather2.render();
+      });
+    toogleMenu();
+  });
+
+  document.getElementById("menu-btn")
+  .addEventListener("click", () => {
+    toogleMenu();
+  });
+}
+
+function toogleMenu(){
+  if(isMenuVisible){
+    sections.menu.classList.add('hidden');
+    sections.location.classList.remove('hidden');
+    isMenuVisible = false;
+  } else {
+    sections.menu.classList.remove('hidden');
+    sections.location.classList.add('hidden');
+    isMenuVisible = true;
+  }
+}
+
+function getLocation() {
+  return new Promise(resolve => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => { 
+          resolve(
+            {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            }
+          );
+        })
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  });
+}
--- js/weather.js
+++ js/weather.js
@@ -2,24 +2,30 @@ class Weather {
   constructor(data) {
     this.container = data.container;
     this.data = data.data;
-    //this.grades = "Kevin";
-    this.grades = "Fahrenheit";
+    this.grades = "celsius";
+    
   }
 
   render(){
     console.log(this.data);
     let temp = this.data.main.temp;
     let feelsLike = this.data.main.feels_like;
-    if(this.grades === "Kevin"){
-      temp = `${this.getCelsius(temp)}°C`;
-      feelsLike = `Feels like: ${this.getCelsius(feelsLike)}°C`;
-    } else if(this.grades === "Fahrenheit"){
-      temp = `${this.getFahrenheit(temp)}°F`;
-      feelsLike = `Feels like: ${this.getFahrenheit(feelsLike)}°F`;
-    }
     let windSpeed = `Wind: ${this.data.wind.speed} K/m`; 
     let humidity = `Humidity levels: ${this.data.main.humidity}%`; 
+
+    if(this.grades === "celsius"){
+      temp = `${this.getCelsius(temp)}°C`;
+      feelsLike = `Feels like: ${this.getCelsius(feelsLike)}°C`;
+    } else if(this.grades === "fahrenheit"){
+      temp = `${this.getFahrenheit(temp)}°F`;
+      feelsLike = `Feels like: ${this.getFahrenheit(feelsLike)}°F`;
+    } else {
+      temp = `${temp}°K`;
+      feelsLike = `Feels like: ${feelsLike}°K`;
+    }
+    
     this.container.querySelector(".name").innerHTML = this.data.name;
+    this.container.querySelector(".country").innerHTML = this.data.sys.country;
     this.container.querySelector(".main-temp").innerHTML = temp;
     this.container.querySelector(".weather-main").innerHTML = this.data.weather[0].main;
     this.container.querySelector(".wind-speed").innerHTML = windSpeed;
@@ -28,7 +34,7 @@ class Weather {
   }
 
   getCelsius(temp){
-    return temp - 273.15;
+    return (temp - 273.15).toFixed(2);
   }
 
   getFahrenheit(temp){
