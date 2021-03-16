d1a9d1051567765dff333dce4a01f2fa13cc7110
Emas <emas.dev@gmail.com>
2021-03-15 19:24:43 -0600

    Fix linter error

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-import { Weather } from "./weather.js";
+/* eslint-disable import/extensions */
+import Weather from './weather.js';
 
-let weather1 = new Weather({
-  container: document.getElementById("weather-mexico")
+const weather1 = new Weather({
+  container: document.getElementById('weather-mexico'),
 });
 
-let weather2 = new Weather({
-  container: document.getElementById("weather-location")
+const weather2 = new Weather({
+  container: document.getElementById('weather-location'),
 });
 
-const appId = "8ca3d179747703e653793ebb8935cbbf";
+const appId = '8ca3d179747703e653793ebb8935cbbf';
 const location = {
-  latitude: 19.363661, 
-  longitude: -99.173382
-}
+  latitude: 19.363661,
+  longitude: -99.173382,
+};
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${appId}`)
   .then(response => response.json())
@@ -21,63 +22,14 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&l
     weather1.render();
   });
 
-window.onload = () => {
-  initEvents();
-}
-
 let isMenuVisible = true;
 const sections = {
-  menu: document.getElementById("section-menu"),
-  location: document.getElementById("section-location"),
-}
+  menu: document.getElementById('section-menu'),
+  location: document.getElementById('section-location'),
+};
 
-function initEvents(){
-
-  const checkboxes = document.getElementsByClassName("radio");
-  for(const checkbox of checkboxes){
-    checkbox.addEventListener("click", (e) => {
-      weather1.grades = e.target.value;
-      weather1.render();
-      weather2.grades = e.target.value;
-      weather2.render();
-    });
-  }
-
-  document.getElementById("get-current-location")
-  .addEventListener("click", () => {
-    getLocation().then(location => {
-      console.log(location);
-      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${appId}`)
-      .then(response => response.json())
-      .then(data => {
-        weather2.data = data;
-        weather2.render();
-      });
-    })
-    toogleMenu();
-  });
-
-  document.getElementById("get-city-location")
-  .addEventListener("click", () => {
-
-      const city = document.getElementById("city-input").value;
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}`)
-      .then(response => response.json())
-      .then(data => {
-        weather2.data = data;
-        weather2.render();
-      });
-    toogleMenu();
-  });
-
-  document.getElementById("menu-btn")
-  .addEventListener("click", () => {
-    toogleMenu();
-  });
-}
-
-function toogleMenu(){
-  if(isMenuVisible){
+function toogleMenu() {
+  if (isMenuVisible) {
     sections.menu.classList.add('hidden');
     sections.location.classList.remove('hidden');
     isMenuVisible = false;
@@ -89,18 +41,68 @@ function toogleMenu(){
 }
 
 function getLocation() {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => { 
-          resolve(
-            {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            }
-          );
-        })
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve(
+          {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          },
+        );
+      });
     } else {
-      alert("Geolocation is not supported by this browser.");
+      reject(new Error('Geolocation is not supported by this browser.'));
     }
   });
 }
+
+function initEvents() {
+  const checkboxes = document.getElementsByClassName('radio');
+  for (let i = 0; i < checkboxes.length; i += 1) {
+    const checkbox = checkboxes[i];
+    checkbox.addEventListener('click', (e) => {
+      weather1.grades = e.target.value;
+      weather1.render();
+      if (weather2.data) {
+        weather2.grades = e.target.value;
+        weather2.render();
+      }
+    });
+  }
+
+  document.getElementById('get-current-location')
+    .addEventListener('click', () => {
+      getLocation().then(location => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${appId}`)
+          .then(response => response.json())
+          .then(data => {
+            weather2.data = data;
+            weather2.render();
+          });
+      });
+      toogleMenu();
+    });
+
+  document.getElementById('get-city-location')
+    .addEventListener('click', () => {
+      const city = document.getElementById('city-input').value;
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}`)
+        .then(response => response.json())
+        .then(data => {
+          weather2.data = data;
+          weather2.render();
+        });
+      toogleMenu();
+    });
+
+  document.getElementById('menu-btn')
+    .addEventListener('click', () => {
+      toogleMenu();
+    });
+}
+
+
+window.onload = () => {
+  initEvents();
+};
--- js/weather.js
+++ js/weather.js
@@ -2,44 +2,43 @@ class Weather {
   constructor(data) {
     this.container = data.container;
     this.data = data.data;
-    this.grades = "celsius";
-    
+    this.grades = 'celsius';
   }
 
-  render(){
-    console.log(this.data);
-    let temp = this.data.main.temp;
+  render() {
+    let { temp } = this.data.main;
     let feelsLike = this.data.main.feels_like;
-    let windSpeed = `Wind: ${this.data.wind.speed} K/m`; 
-    let humidity = `Humidity levels: ${this.data.main.humidity}%`; 
+    const windSpeed = `Wind: ${this.data.wind.speed} K/m`;
+    const humidity = `Humidity levels: ${this.data.main.humidity}%`;
 
-    if(this.grades === "celsius"){
+    if (this.grades === 'celsius') {
       temp = `${this.getCelsius(temp)}°C`;
       feelsLike = `Feels like: ${this.getCelsius(feelsLike)}°C`;
-    } else if(this.grades === "fahrenheit"){
+    } else if (this.grades === 'fahrenheit') {
       temp = `${this.getFahrenheit(temp)}°F`;
       feelsLike = `Feels like: ${this.getFahrenheit(feelsLike)}°F`;
     } else {
       temp = `${temp}°K`;
       feelsLike = `Feels like: ${feelsLike}°K`;
     }
-    
-    this.container.querySelector(".name").innerHTML = this.data.name;
-    this.container.querySelector(".country").innerHTML = this.data.sys.country;
-    this.container.querySelector(".main-temp").innerHTML = temp;
-    this.container.querySelector(".weather-main").innerHTML = this.data.weather[0].main;
-    this.container.querySelector(".wind-speed").innerHTML = windSpeed;
-    this.container.querySelector(".main-feels_like").innerHTML = feelsLike;
-    this.container.querySelector(".main-humidity").innerHTML = humidity;
+
+    this.container.querySelector('.name').innerHTML = this.data.name;
+    this.container.querySelector('.country').innerHTML = this.data.sys.country;
+    this.container.querySelector('.main-temp').innerHTML = temp;
+    this.container.querySelector('.weather-main').innerHTML = this.data.weather[0].main;
+    this.container.querySelector('.wind-speed').innerHTML = windSpeed;
+    this.container.querySelector('.main-feels_like').innerHTML = feelsLike;
+    this.container.querySelector('.main-humidity').innerHTML = humidity;
   }
 
-  getCelsius(temp){
+  /* eslint-disable class-methods-use-this */
+  getCelsius(temp) {
     return (temp - 273.15).toFixed(2);
   }
 
-  getFahrenheit(temp){
-    return (this.getCelsius(temp) * (9/5 + 32)).toFixed(2);
+  getFahrenheit(temp) {
+    return (this.getCelsius(temp) * (9 / 5 + 32)).toFixed(2);
   }
 }
 
-export { Weather }
+export default Weather;
